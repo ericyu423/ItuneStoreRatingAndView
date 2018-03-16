44a71096f17e89e9cbed508708ba7223737ecb80
ericyu423 <ericyu423@hotmail.com>
2018-03-16 08:20:07 -0700

    Initial Commit

---
+++ ItuneStoreRatingAndView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ItuneStoreRatingAndView
+//
+//  Created by eric yu on 3/16/18.
+//  Copyright © 2018 eric yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ItuneStoreRatingAndView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ItuneStoreRatingAndView
+//
+//  Created by eric yu on 3/16/18.
+//  Copyright © 2018 eric yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
